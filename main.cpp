fb148f201da6cfea1ddbf53aeebda94ff5509c0e
Lari <lnorri@gmail.com>
2026-06-19 12:26:16 -0400

    WIP on main
    Added a more interesting example effect and made the CMakeLists.txt starting point more helpful for Mac users

--- main.cpp
+++ main.cpp
@@ -17,42 +17,40 @@ using namespace CORE;
 using namespace SYSTEM;
 using namespace GRAPHICS;
 
+// Example visual effects
+void TV_Static(unsigned int* pixels, unsigned int width, unsigned int height);
+void MovingWaveform(float totalTime, unsigned int* pixels, unsigned int width, unsigned int height);
+
 int main() {
     cout << "Programming Challenge A. Let's Go!!!" << endl;
 
     GWindow window;
     if (+window.Create(100, 100, 800, 600, GWindowStyle::WINDOWEDLOCKED)) {
-
         GRasterSurface surface;
         if (+surface.Create(window)) {
-
             int frames = 0;
+            float time = 0;
             auto start = std::chrono::high_resolution_clock::now();
+            const auto initalTime = start;
             while (+window.ProcessWindowEvents()) {
                 // known issue: Clear() does not affect Present() unless Update() calls occur.
                 if (+surface.Clear(0xFF000000)) {
-
                     unsigned int *pixels;
                     unsigned short width, height;
                     if (+surface.LockUpdateBufferWrite(&pixels, width, height)) {
-
+                        auto end = std::chrono::high_resolution_clock::now();
+                        std::chrono::duration<float> total = end - initalTime;
+                        
                         // This is where we can update the surface
-                        for (unsigned int y = 0; y < height; ++y) {
-                            for (unsigned int x = 0; x < width; ++x) {
-                                unsigned int noise = rand() % 256;
-                                pixels[y * width + x] = 0xFF000000 | noise | noise << 8 | noise << 16;
-                            }
-                        }
+                        MovingWaveform(total.count(), pixels, width, height);
+                        //TV_Static(pixels, width, height);
 
                         surface.UnlockUpdateBufferWrite();
                     }
                     surface.Present();
-
                     auto end = std::chrono::high_resolution_clock::now();
-                    std::chrono::duration<double> elapsed = end - start;
-
-                    if (elapsed.count() > 1.0) {
-
+                    std::chrono::duration<float> elapsed = end - start;
+                    if (elapsed.count() > 1.0f) {
                         char buffer[128];
                         sprintf(buffer, "[Your Project/Title/Name Here] FPS %i", frames);
                         window.SetWindowName(buffer);
@@ -60,14 +58,32 @@ int main() {
                         frames = 0;
                     }
                     else ++frames;
-
                 }
-
             }
-
         }
-
     }
-
     return 0;
+}
+
+void TV_Static(unsigned int* pixels, unsigned int width, unsigned int height) {
+    for (unsigned int y = 0; y < height; ++y) {
+        for (unsigned int x = 0; x < width; ++x) {
+            unsigned int noise = rand() % 256;
+            pixels[y * width + x] = 0xFF000000 | noise | noise << 8 | noise << 16;
+        }
+    }
+}
+
+void MovingWaveform(float totalTime, unsigned int* pixels, unsigned int width, unsigned int height) {
+    for (unsigned y = 0; y < height; ++y) {
+        for (unsigned x = 0; x < width; ++x) {
+            // plot wave when close
+            float amp = height>>2, freq = 1, h_shift = totalTime, v_shift = height>>2;
+            float Yexpected = (height>>2) + amp * sinf(freq * (G_DEGREE_TO_RADIAN_F(x) - h_shift)) + v_shift;
+            if (G_ABSOLUTE_COMPARISON(y, Yexpected, 100))
+                pixels[y * width + x] = 0xFFFF6E00; // fullsail orange
+            else
+                pixels[y * width + x] = 0xFFFFFFFF; // white background
+        }
+    }
 }
---
+++ main.mm
@@ -0,0 +1,2 @@
+// This is how we "trick" AppleClang into using primairily C++ code
+#include "main.cpp"
